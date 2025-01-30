aa728cccc88c42488cb6370d7e0fccdeca5e37a6
surya74697873 <surya74697873@gmail.com>
2025-01-31 00:19:38 +0530

    Fix Vercel 404 issue

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    port : 1111,
-  },
   base : '/',
   build : {
     outDir : 'dist',
